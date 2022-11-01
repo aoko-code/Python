5b0306846d738bbc8484f1b252a879330ca99ea7
aoko-code <cynthiakhamenge@yahoo.com>
2022-11-01 15:18:45 +0300

    dilate and erode

--- openCv/convertImg.py
+++ openCv/convertImg.py
@@ -11,5 +11,12 @@ cv.imshow('grey', gray)
 blur = cv.GaussianBlur(img, (7, 7), cv.BORDER_DEFAULT)
 cv.imshow('blur', blur)
 #edge cascade
-
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('canny', canny)
+#dilate img
+dilated = cv.dilate(canny, (3, 3), iterations= 1)
+cv.imshow('dilated', dilated)
+#eroding dilated image
+eroded = cv.erode(dilated, (3, 3), iterations= 1)
+cv.imshow('eroded', eroded)
 cv.waitKey(0)
