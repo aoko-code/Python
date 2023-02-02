f9d0e3a4c74d92ce7faae03caa20a289c90ccb5d
aoko-code <cynthiakhamenge@yahoo.com>
2023-02-02 11:48:32 +0300

    using modules

---
+++ Bitwise/main.py
@@ -0,0 +1,9 @@
+import bits
+print(bits.NOT(0b0101))
+print(bin(bits.NOT(0b0101)))
+print(bin(bits.NOT(0b0101) & 0xF))
+print(bin(bits.AND(0b0101, 0b0011) & 0xF)) 
+print(bin(bits.AND(0b0101, 0b0100) & 0xF))
+print(bin(bits.OR(0b0101, 0b0100) & 0xF))
+print(bin(bits.OR(0b0101, 0b0011) & 0xF))
+print(bin(bits.XOR(0b0101, 0b11) & 0xF))
