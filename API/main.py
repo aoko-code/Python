74290694abe9d2d3158d989571235d532cbf371e
aoko-code <cynthiakhamenge@yahoo.com>
2023-02-08 15:19:06 +0300

    posts

--- API/main.py
+++ API/main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my API"}
 
-@app.get("/post")
+@app.get("/posts")
 def get_posts():
     return {"data": "this is your post"}
 
-@app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"message": "this is my first post"}
+@app.post("/posts")
+def create_post(post: Post):
+    print(post.dict())
+    return {"data": post}
+
