3688f0405f703c0319a96af61dbc9fc30d774e4b
aoko-code <cynthiakhamenge@yahoo.com>
2023-03-21 22:30:03 +0300

    numpy and jupyter

--- List Comprehension/test.py
+++ List Comprehension/test.py
@@ -1,1 +1,5 @@
-print(list(map((lambda x: x * 2), range(1,11))))
+#newlist = [expression for item in iterable if condition == True]
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
