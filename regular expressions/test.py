664fdbc13edd7b5f3358ecbb652249ccf20c7158
aoko-code <cynthiakhamenge@yahoo.com>
2023-04-17 20:37:16 +0300

    regex definition

---
+++ regular expressions/test.py
@@ -0,0 +1,5 @@
+#used to:
+#   search for a specific string in a large amount of data
+#   verify that a string has a proper format (Email, phone)
+#   find a string and replace it with another string
+#   format data into the proper form for importing for example
