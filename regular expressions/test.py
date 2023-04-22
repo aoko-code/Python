4856b1740e9a2bcb064d9cef87df084e7384d5af
aoko-code <cynthiakhamenge@yahoo.com>
2023-04-22 18:39:24 +0300

    regex

--- regular expressions/test.py
+++ regular expressions/test.py
@@ -3,3 +3,14 @@
 #   verify that a string has a proper format (Email, phone)
 #   find a string and replace it with another string
 #   format data into the proper form for importing for example
+
+import re
+
+if re.search("ape.", "The ape was at the apex"):
+    print("There is an ape")
+theStr = "The ape was at the apex"
+
+for i in re.finditer("ape.", theStr):
+    locTuple = i.span()
+    print(locTuple)
+    print(theStr[locTuple[0]: locTuple[1]])
